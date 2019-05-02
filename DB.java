27a33098234a724b1a8fc0b72fbfed01e47822c3
Himanshu Agarwal <46684300+rock-himanshu@users.noreply.github.com>
2019-05-02 17:10:13 +0530

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	public static Connection getConnection(){
+		Connection con=null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:8888/LMS","root","");
+			System.out.println("Connection is succesfull");
+		}catch(Exception e){System.out.println(e);}
+		return con;
+	}
+
+}
